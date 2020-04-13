b380d2042a4c9d5d4ff555c7a0a8b64d5ce017b6
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-13 03:41:12 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,9 +1,9 @@
 class Dog 
   
   def initialize(name, breed, age)
-    @name = name
-    @breed = breed
-    @age = age
+    self.name = name
+    self.breed = breed
+    self.age = age
   end
   
 end
